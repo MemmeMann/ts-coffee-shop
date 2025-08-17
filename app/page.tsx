1d828a23adb6fdc5e775e76ba54934e3038d9146
Nicola Tino <57228809+MemmeMann@users.noreply.github.com>
2025-08-17 19:55:43 +0200

    Changed fundemental code. Did NOT add any own code. (Reminder for future changes in website)

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,23 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          className={styles.center}
+          src="/The_Big_Bean_Logo.svg"
+          alt="The_Big_Bean_Logo"
+          width={150}
+          height={170}
           priority
         />
         <ol>
           <li>
-            Get started by editing <code>app/page.tsx</code>.
+            Best Coffee in the world!
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Check out our vast inventory of products.</li>
         </ol>
 
         <div className={styles.ctas}>
           <a
             className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -34,60 +33,20 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Menu
           </a>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
             className={styles.secondary}
           >
-            Read our docs
+            About us
           </a>
         </div>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+        <a>
+          Established 2025
         </a>
       </footer>
     </div>
